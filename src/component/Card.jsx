964c1fae2adcc185f01f5eff1790cdee7f980191
root <132725436+tingtung46@users.noreply.github.com>
2024-10-11 16:29:08 +0700

    Return html element

---
+++ src/component/Card.jsx
@@ -0,0 +1,10 @@
+export const Card = ({ handleClick }) => {
+  return (
+    <>
+      <div className="card" onClick={handleClick}>
+        <img src="" alt="" />
+        <p></p>
+      </div>
+    </>
+  );
+};
