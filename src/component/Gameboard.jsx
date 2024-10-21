fc5c3f3ef099c991c64dc0d4a39e5edacfee61d5
root <132725436+tingtung46@users.noreply.github.com>
2024-10-21 18:14:34 +0700

    Get characters to be fetched

--- src/component/Gameboard.jsx
+++ src/component/Gameboard.jsx
@@ -1,6 +1,55 @@
-import Card from "./Card";
+import { Card } from "./Card";
+import { useEffect, useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const Gameboard = () => {
+  const [characters, setCharacters] = useState();
+
+  const charasID = useMemo(
+    () => [
+      "63851",
+      "63845",
+      "65541",
+      "73203",
+      "73205",
+      "73227",
+      "73235",
+      "65523",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getCharas = async (charasID) => {
+      try {
+        const promises = charasID.map(async (id) => {
+          const res = await fetch(
+            `https://api.jikan.moe/v4/characters/${id}/full`,
+            { mode: "cors" }
+          );
+
+          return await res.json();
+        });
+
+        const charasData = await Promise.all(promises);
+
+        const allChara = charasData.map((chara) => ({
+          id: uuidv4(),
+          mal_id: chara.data.mal_id,
+          name: chara.data.name,
+          image: chara.data.images.jpg.image_url,
+          visited: false,
+        }));
+
+        setCharacters(allChara);
+      } catch (error) {
+        return console.error("Fetch failed", error);
+      }
+    };
+
+    getCharas(charasID);
+  }, [charasID]);
+
   return (
     <>
       <section className="gameboard">
@@ -10,7 +59,9 @@ export const Gameboard = () => {
         </div>
 
         <div className="cards-container">
-          <Card />
+          {characters.map((chara) => {
+            return <Card chara={chara} key={chara.id} />;
+          })}
         </div>
       </section>
     </>
