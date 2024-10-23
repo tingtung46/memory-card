33f8c2d130a0d75f750ead5b66d7fcdf2c201fc2
root <132725436+tingtung46@users.noreply.github.com>
2024-10-23 21:17:40 +0700

    Update props and html attribute

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,9 +1,9 @@
-export const Card = ({ handleClick }) => {
+export const Card = ({ handleClick, chara }) => {
   return (
     <>
-      <div className="card" onClick={handleClick}>
-        <img src="" alt="" />
-        <p></p>
+      <div className="card" onClick={handleClick} data-id={chara.mal_id}>
+        <img src={chara.image} alt={chara.name} />
+        <p>{chara.name}</p>
       </div>
     </>
   );
