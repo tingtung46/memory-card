4d7c2ee73f834be281213bd132b201881e5dd79c
root <132725436+tingtung46@users.noreply.github.com>
2024-11-14 06:51:08 +0700

    Update main CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="m-0 bg-no-repeat bg-cover bg-center bg-fixed">
+    <main className="m-0 bg-no-repeat bg-cover bg-center bg-fixed flex place-items-center justify-center">
       <Gameboard />
     </main>
   );
